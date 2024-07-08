8fa91a38697629da68cba3ee88e7205eb34297d2
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-08 23:23:23 +0330

    update gravity and jump to stuck on the wall

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed; // add speed to move player faster , make serializefield to edit directly from unity( varibale speed in player )
+    [SerializeField] private float jPower;
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask wallLayer;
 
     private Rigidbody2D body; //create refrence to body of char
 
      private Animator anim; //create refrence to animation of char
 
      private BoxCollider2D boxCollider;
+
+     private float wallJump;
+
+     private float horizontalInput;
     
     //everytime the scripts loaded , get and store the component in body variable
     private void Awake()
@@ -22,9 +28,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        body.velocity=new Vector2(horizontalInput*speed,body.velocity.y); //input.getaxis ->left key = -1 and right key =1
+        horizontalInput = Input.GetAxis("Horizontal");
 
         if(horizontalInput > 0.01f) //flip character to the right
         {
@@ -34,20 +38,64 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-5,5,5);
         }
-
-        if(Input.GetKey(KeyCode.Space) && isGrounded()) // if the key is space and character is on the ground then jump
-            jump();
-           
+  
         anim.SetBool("run", horizontalInput != 0); // if it moves ->  bool run = true , if not -> bool run = false ;
         anim.SetBool("grounded",isGrounded()); // initial this bool with the boolean variable grounded in the code
     
+    // wall logic
+        if(wallJump > 0.2f )
+        {
+          
+             body.velocity=new Vector2(horizontalInput*speed,body.velocity.y); //input.getaxis ->left key = -1 and right key =1
+
+             if(onWall() && !isGrounded()) 
+             {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero; // if player is on wall -> stuck and don't able to fall down
+             }
+             else
+             {
+                body.gravityScale = 7; // if char is not on wall then fall
+             }
+
+            if(Input.GetKey(KeyCode.Space)) // if the key is space and character is on the ground then jump
+            jump();
+
+
+        }
+        else 
+        {
+
+            wallJump += Time.deltaTime; /// ??????
+        }
     }
 
     private void jump()
     {
-         body.velocity=new Vector2(body.velocity.x,speed); // jump with space key
+        if(isGrounded()){
+
+         body.velocity=new Vector2(body.velocity.x,jPower); // jump with space key
          
          anim.SetTrigger("jump");
+        }
+
+        else if (onWall() && !isGrounded())
+        {
+            if(horizontalInput == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+
+            }
+            else
+            {
+                body.velocity= new Vector2(-Mathf.Sign(transform.localScale.x)*3,6);
+
+            }
+            wallJump = 0; // wait a bit before next jump
+
+        }
+        
 
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -58,7 +106,10 @@ public class PlayerMovement : MonoBehaviour
     private bool isGrounded(){ 
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0,Vector2.down,0.1f,groundLayer);  //make virtual line(arrow to down from char feet) from a point origin to certain direction , if it intersects with an object with collider on it -> true , if not -> false
         return raycastHit.collider != null;                                  // we use box with width of player - > we can now know if player is actually on the ground or not
-    }                                                   // 1 : origin , 2 : size , 3 : angle , 4: direction , 5 : position of virtual box(distance) , 6: layer mask
+    }                                                   // 1 : origin , 2 : size , 3 : angle , 4: direction , 5 : position of virtual box(distance) , 6: layer mask (what object we want to detect)
 
-    
+    private bool onWall(){ 
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0,new Vector2(transform.lossyScale.x,0),0.1f,wallLayer);
+        return raycastHit.collider != null;                                 
+    }       
 }
