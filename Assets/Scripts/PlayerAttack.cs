6ee8acb6ef47a89af1f6153daa956b6f605420eb
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-09 01:28:38 +0330

    added some assets for fireBall and its explosion

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   [SerializeField] private float nextAttackTime; // how much time should spend between 2 attacks
+   private Animator anim;
+   private PlayerMovement playerMovement;
+   private float coolDownTimer = math.INFINITY; // if we don't set it player can't attack at first of the game
+
+   private void Awake()
+   {
+        anim=GetComponent<Animator>();
+        playerMovement=GetComponent<PlayerMovement>();
+   }
+    // Update is called once per frame
+    private void Update()
     {
-        
+        if(Input.GetMouseButton(0) && coolDownTimer > nextAttackTime && playerMovement.canAttack())
+        {
+            Attack();
+        }
+        coolDownTimer += Time.deltaTime; // update every frame
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Attack()
     {
-        
+        anim.SetTrigger("attack");
+        coolDownTimer = 0;
     }
 }
