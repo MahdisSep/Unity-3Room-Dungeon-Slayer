bbf2583a7f111309ef85e10142b458b0a18cda4c
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-08 17:38:58 +0330

    adjust speed of movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed; // add speed to move player faster , make serializefield to edit directly from unity( varibale speed in player )
     private Rigidbody2D body; //create refrence to it
     
     //everytime the scripts loaded , get and store the component in body variable
@@ -14,8 +14,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        body.velocity=new Vector2(Input.GetAxis("Horizontal")*speed,body.velocity.y);
-        if(Input.GetKey(KeyCode.Space))
-            body.velocity=new Vector2(body.velocity.x,speed);
+        body.velocity=new Vector2(Input.GetAxis("Horizontal")*speed,body.velocity.y); //input.getaxis ->left key = -1 and right key =1
+        // if(Input.GetKey(KeyCode.Space))
+        //     body.velocity=new Vector2(body.velocity.x,speed);
     }
 }
