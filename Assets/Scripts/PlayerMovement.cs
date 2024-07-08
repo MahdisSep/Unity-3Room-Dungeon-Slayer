f3200c273fd4a0af2fd7f32c8095db5f37e65dbd
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-08 17:13:29 +0330

    body refrence to rigidbody

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D body; //create refrence to it
+    
+    //everytime the scripts loaded
+    private void Awake()
     {
-        
+        body=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        body.velocity=new Vector2(Input.GetAxis("Horizontal")*speed,body.velocity.y);
+        if(Input.GetKey(KeyCode.Space))
+            body.velocity=new Vector2(body.velocity.x,speed);
     }
 }
