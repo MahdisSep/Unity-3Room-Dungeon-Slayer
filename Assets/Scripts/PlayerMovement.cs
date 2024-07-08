9dc93f6e5e79f03ac2ccb9eabc600790fb4b0371
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-08 17:15:02 +0330

    using getcomponent to get the body

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D body; //create refrence to it
     
-    //everytime the scripts loaded
+    //everytime the scripts loaded , get and store the component in body variable
     private void Awake()
     {
         body=GetComponent<Rigidbody2D>();
