f048e122b9a916c2079590a0c95d32fab1ea73f2
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-09 02:01:25 +0330

    update projectile code to transition between shooting and exploding

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,17 +1,55 @@
 
+using System;
 using UnityEngine;
 
 public class Project : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    private bool hit;
+    private BoxCollider2D boxCollider;
+    private Animator anim;
+    private float direction; //which direction the fireball will fly
+
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(hit) return;
+        float movementSpeed = speed * Time.deltaTime*direction;
+        transform.Translate(movementSpeed,0,0); // just x asix changed ???
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //check if fireball hit any objects
+    {
+        hit = true;
+        boxCollider.enabled = false; ///???
+        anim.SetTrigger("explode");
+    }
+
+    public void SetDirection(float _direction) // call this method everytime we shoot for choosing the direction left or right
+    {
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false ;
+        boxCollider.enabled = true;
+
+// for changing the direction of fireball(not nessecery for us)
+
+        float localScaleX = transform.localScale.x;
+        if (Math.Sign(localScaleX) != _direction)
+        {
+            localScaleX = - localScaleX;
+        }
+        transform.localScale = new Vector3(localScaleX,transform.localScale.y,transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false); // deactivate the fireball after explosion animation finished.
     }
 }
