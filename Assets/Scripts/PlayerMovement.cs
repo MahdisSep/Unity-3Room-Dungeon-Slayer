730838743fc988f06a94e856ef264cd59eed22c8
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-08 22:34:48 +0330

    update jumping and fix gravity scale

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed; // add speed to move player faster , make serializefield to edit directly from unity( varibale speed in player )
+    [SerializeField] LayerMask groundLayer;
+
     private Rigidbody2D body; //create refrence to body of char
 
      private Animator anim; //create refrence to animation of char
 
-     private bool grounded;
+     private BoxCollider2D boxCollider;
     
     //everytime the scripts loaded , get and store the component in body variable
     private void Awake()
     {
         body=GetComponent<Rigidbody2D>();
         anim =GetComponent<Animator>();
+        boxCollider=GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -32,23 +35,30 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-5,5,5);
         }
 
-        if(Input.GetKey(KeyCode.Space) && grounded) // if the key is space and character is on the ground then jump
+        if(Input.GetKey(KeyCode.Space) && isGrounded()) // if the key is space and character is on the ground then jump
             jump();
            
         anim.SetBool("run", horizontalInput != 0); // if it moves ->  bool run = true , if not -> bool run = false ;
-        anim.SetBool("grounded",grounded); // initial this bool with the boolean variable grounded in the code
+        anim.SetBool("grounded",isGrounded()); // initial this bool with the boolean variable grounded in the code
     
     }
 
     private void jump()
     {
          body.velocity=new Vector2(body.velocity.x,speed); // jump with space key
-         grounded = false ; // if char jumps , then grounded = false
+         
+         anim.SetTrigger("jump");
 
     }
-     private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground") // if the collision to the other objects with tag ground , then set grounded true.
-            grounded = true;
+            
     }
+
+    private bool isGrounded(){ 
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0,Vector2.down,0.1f,groundLayer);  //make virtual line(arrow to down from char feet) from a point origin to certain direction , if it intersects with an object with collider on it -> true , if not -> false
+        return raycastHit.collider != null;                                  // we use box with width of player - > we can now know if player is actually on the ground or not
+    }                                                   // 1 : origin , 2 : size , 3 : angle , 4: direction , 5 : position of virtual box(distance) , 6: layer mask
+
+    
 }
