0f4ee16001c4022020d9fe3dc9645c5c81f7d748
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-09 22:49:38 +0330

    new object door

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed ; //speed of camera's movement
+    private float currentPosX; //position of camera
+    private Vector3 velocity = Vector3.zero;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+private void Update()
+{
+    transform.position = Vector3.SmoothDamp(transform.position,new Vector3(currentPosX , transform.position.y , transform.position.z),ref velocity , speed * Time.deltaTime);
+    //1-> current position of the camera , 2 -> destination  , 3 -> velocity vector , 4-> speed of the movement ( * delta time to frame independent)
+}
+
+public void MoveToNewRoom(Transform _newRoom)
+{
+    currentPosX = _newRoom.position.x; //when we enter new room we should change the position of the camera to the new room
+}
+
+
+
 }
