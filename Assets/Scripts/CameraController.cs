5e3b1870dffb673b92deb941c459a690ab0f5b0e
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-09 23:55:28 +0330

    updated camera from room camera to player camera , also added lookahead

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,10 +7,20 @@ public class CameraController : MonoBehaviour
     private float currentPosX; //position of camera
     private Vector3 velocity = Vector3.zero;
 
+    //player camera
+    [SerializeField] private Transform player;
+    [SerializeField] private float lookForwardDistance; // how much far camera can look forward
+    [SerializeField] private float camSpeed ; //different from above
+    private float lookahead;
+
 private void Update()
 {
-    transform.position = Vector3.SmoothDamp(transform.position,new Vector3(currentPosX , transform.position.y , transform.position.z),ref velocity , speed );
+    //room camera
+   // transform.position = Vector3.SmoothDamp(transform.position,new Vector3(currentPosX , transform.position.y , transform.position.z),ref velocity , speed );
     //1-> current position of the camera , 2 -> destination  , 3 -> velocity vector , 4-> speed of the movement ( * delta time to frame independent)
+    transform.position = new Vector3 (player.position.x + lookahead , transform.position.y , transform.position.z);
+    //better player camera (to have limit value to look forward)
+    lookahead = Mathf.Lerp(lookahead, (lookForwardDistance * player.localScale.x), Time.deltaTime * camSpeed);
 }
 
 public void MoveToNewRoom(Transform _newRoom)
