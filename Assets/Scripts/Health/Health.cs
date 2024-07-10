ca327b1b1756f6cd0fd8ab167cfb0f5b44e90f44
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-10 19:52:48 +0330

    added iframes

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,17 +1,25 @@
-
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float initialHealth;
     public float currentHealth {get ; private set;} //only can change in this script but we can get it in another scripts
     private Animator anim;
     private bool dead;
 
+    [Header ("iFrames")]
+    [SerializeField] private float invalunarabilityTime;
+    [SerializeField] private int numOfFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = initialHealth ;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeHurt(float _damage)
@@ -22,6 +30,7 @@ public class Health : MonoBehaviour
         {
             //player hurt
             anim.SetTrigger("hurt");
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -48,4 +57,23 @@ public class Health : MonoBehaviour
     //         TakeHurt(1); // everytime we tap E red heart gonna disapeared -> just for testing
     //     }
     // }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11,true);
+
+        //invulnerability duration
+
+        for (int i = 0 ; i < numOfFlashes ; i++)
+        {
+            spriteRend.color = new Color(1,0,0,0.4f); //after collision with enemy player'color will be changed.
+            yield return new WaitForSeconds(invalunarabilityTime / (numOfFlashes *2)); //wait a few seconds and then execute next line, ( for example 2 seconds  -> 3 times go to red and then back)
+            spriteRend.color = Color.white; //change the color of player to the original.
+            yield return new WaitForSeconds(invalunarabilityTime / (numOfFlashes *2)); //wait a few seconds and then execute next line.
+
+        }
+
+        Physics2D.IgnoreLayerCollision(10,11,false);
+    }
+
 }
