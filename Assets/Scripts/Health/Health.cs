1119e5f88b847c6f4d844473bacd76ba3a35d198
MahdisSep <168344791+MahdisSep@users.noreply.github.com>
2024-07-10 12:35:04 +0330

    update health

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float initialHealth;
+    private float currentHealth;
+
+    private void Awake()
     {
-        
+        currentHealth = initialHealth ;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeHurt(float _damage)
     {
-        
+        currentHealth = Mathf.Clamp(currentHealth - _damage , 0, initialHealth);
+
+        if (currentHealth > 0)
+        {
+            //player hurt
+        }
+        else
+        {
+            //player dead
+        }
     }
 }
